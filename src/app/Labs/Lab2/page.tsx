33550b41983416e4653498ff7b1084bf6457f30e
Chris <125088905+wyattchris@users.noreply.github.com>
2025-10-03 22:19:45 -0400

    fix apostraphes

--- src/app/Labs/Lab2/page.tsx
+++ src/app/Labs/Lab2/page.tsx
@@ -9,7 +9,7 @@ export default function Lab2() {
       <h3>Styling with the STYLE attribute</h3>
       <p>
         Style attribute allows configuring look and feel right on the element.
-        Although it's very convenient it is considered bad practice and you
+        Although it&apos;s very convenient it is considered bad practice and you
         should avoid using the style attribute
       </p>
       <div id="wd-css-id-selectors">
@@ -19,7 +19,7 @@ export default function Lab2() {
           name, e.g., P, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
-          Here's another paragraph using a different ID and a different look and
+          Here&apos;s another paragraph using a different ID and a different look and
           feel
         </p>
       </div>
@@ -27,7 +27,7 @@ export default function Lab2() {
         <h3>Class selectors</h3>
 
         <p className="wd-class-selector">
-          Instead of using IDs to refer to elements, you can use an element's
+          Instead of using IDs to refer to elements, you can use an element&apos;s
           CLASS attribute
         </p>
 
@@ -42,7 +42,7 @@ export default function Lab2() {
             Selectors can be combined to refer elements in particular places in
             the document
             <p className="wd-selector-3">
-              This paragraph's red background is referenced as
+              This paragraph&apos;s red background is referenced as
               <br />
               .selector-2 .selector3
               <br />
